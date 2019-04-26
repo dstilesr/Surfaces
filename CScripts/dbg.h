6b637e91b5787975262f189669ee081fd619f8e1
dstilesr <d.stiles.r@hotmail.com>
2019-04-25 20:14:31 -0500

    Streamline Julia code

--- CScripts/dbg.h
+++ CScripts/dbg.h
@@ -19,8 +19,8 @@
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
 #define log_err(M, ...) fprintf(stderr,\
-    "[ERROR](%s:%d: errno: %s) " M "\n", __FILE__, __LINE__,\
-    clean_errno(), ##__VA_ARGS__)
+    "[ERROR](%s:%d:%s: errno: %s) " M "\n", __FILE__, __LINE__,\
+    __FUNCTION__, clean_errno(), ##__VA_ARGS__)
 
 #define log_warn(M, ...) fprintf(stderr,\
     "[WARN] (%s:%d: errno: %s) " M "\n",\
