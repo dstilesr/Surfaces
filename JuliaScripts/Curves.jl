b079bad83c84a450e85721ed52b6dd8a027bf68c
David Stiles <41964604+dstilesr@users.noreply.github.com>
2019-10-09 07:02:22 -0500

    Develop (#4)
    * Simplify signtest macro
    
    * Add Trajectories.jl file
    
    * Add files via upload
    
    * Example
    
    * First version of vector field and trajectory functions
    
    * Add curves file
    
    * Modify curve length function to use only one loop
    
    * Update vector field scripts
    
    * Fix parameters in trajectory function
    
    * Update Trajectories.jl
    
    * Add files via upload
    
    The scripts to find umbilical points of a distribution (non holonomic surface) and of a surface given by implicit equation
    
    * Umbilical points
    
    To find umbilical points of a surface given by implicit equation
    
    * Much faster and interactive
    
    Much faster by calculating principal curvatures as eigenvalues, and now interactive :)
    
    * Rename nh5.jl to JuliaScripts/nh5.jl
    
    * Much faster and interactive
    
    Much faster by computing principal curvatures as eigenvalues, and now it is interactive
    
    * Much faster, save files to use with gnuplot, interactive
    
    * Add quaternions file for rotations
    
    * Add simple convergence test for curves
    
    * Ammend syntax
    
    * changed some details
    
    * uses second derivatives found by hand
    
    * Script puntos criticos lagrange

--- JuliaScripts/Curves.jl
+++ JuliaScripts/Curves.jl
@@ -15,3 +15,32 @@ function curvelength(curve::Array{Array{T,1},1}) where {T <: AbstractFloat}
 	
 	return len
 end
+
+
+"""Uses a simple Cauchy criterion to determine whether a curve (given as an 
+array of points) converges as the index increases. Returns a boolean value that
+tells whether convergence was found and an array (the point of convergence if found,
+the last point on the curve if not)."""
+function curveCauchy(curve::Array{Array{T,1},1}, ϵ::T=0.0001, start::Int64=10) where {T <: AbstractFloat}
+	n::Int64 = length(curve)
+	n <= start && error("Start index is out of curve bounds.")
+	
+	i::Int64 = start
+	done::Bool = false
+	converged::Bool = false
+	diff::Array{T,1} = Array{T,1}(undef, length(curve[1]))
+	
+	while !done
+		diff = curve[i] - curve[i-1]
+		if sqrt(dot(diff,diff)) < ϵ
+			done = true
+			converged = true
+		elseif i == n
+			done = true
+		else
+			i += 1
+		end
+	end
+	
+	return converged, curve[i]
+end
---
+++ JuliaScripts/Quaternions.jl
@@ -0,0 +1,125 @@
+# Implementation of quaternions and functions to rotate points in R3
+
+# Base functions to be extended
+import Base.+, Base.*, Base.conj, Base.inv, Base.zero, Base.one, Base.-, Base.==
+import Base.show, Base./, Base.isapprox
+
+#-------------------------------------------------------------------------------
+
+"""Define Quaternions"""
+mutable struct Quaternion{T <: Real}
+    scalar :: T
+    x :: T
+    y :: T
+    z :: T
+end
+
+# Basic operations on quaternions
+
+"""Equality of Quaternions"""
+==(a::Quaternion, b::Quaternion) = (a.scalar==b.scalar)&&(a.x==b.x)&&(a.y==b.y)&&(a.z==b.z);
+
+"""Approximate equality of quaternions"""
+isapprox(a::Quaternion, b::Quaternion) = isapprox(a.scalar,b.scalar)&&isapprox(a.x,b.x)&&isapprox(a.y,b.y)&&isapprox(a.z,b.z)
+
+"""Quaternion output on screen"""
+function show(io::IO, a::Quaternion)
+    println("$(a.scalar) + $(a.x)i + $(a.y)j + $(a.z)k")
+end
+
+"""We can add Quaternions of the same type"""
++(a::Quaternion{T}, b::Quaternion{T}) where {T <: Real} = Quaternion{T}(a.scalar + b.scalar,
+    a.x + b.x, a.y + b.y, a.z + b.z);
+
+"""Add a quaternion and a real number"""
++(a::Quaternion{T}, r::T) where {T <: Real} = Quaternion{T}(a.scalar + r, a.x, a.y, a.z);
++(r::T, a::Quaternion{T}) where {T <: Real} = +(a,r);
+
+"""Subtract quaternions of the same type"""
+-(a::Quaternion{T}, b :: Quaternion{T}) where {T <: Real} = Quaternion{T}(a.scalar - b.scalar,
+    a.x - b.x, a.y - b.y, a.z - b.z)
+
+-(a::Quaternion{T}) where {T <: Real} = Quaternion{T}(-a.scalar, -a.x, -a.y, -a.z)
+
+"""Conjugate a Quaternion"""
+conj(a::Quaternion{T}) where {T <: Real} = Quaternion{T}(a.scalar, -a.x, -a.y, -a.z);
+
+function conj!(a::Quaternion{T}) where {T <: Real}
+	a.scalar *= -1
+	a.x *= -1
+	a.y *= -1
+	a.z *= -1;
+end
+
+"""Quaternion product"""
+*(a::Quaternion{T}, b::Quaternion{T}) where {T <: Real} = Quaternion{T}(
+    a.scalar*b.scalar - a.x*b.x - a.y*b.y - a.z*b.z,
+    a.scalar*b.x + a.x*b.scalar + a.y*b.z - a.z*b.y,
+    a.scalar*b.y - a.x*b.z + a.y*b.scalar + a.z*b.x,
+    a.scalar*b.z + a.x*b.y - a.y*b.x + a.z*b.scalar
+);
+
+"""Multiply quaternion by real number"""
+*(a::Quaternion{T}, r::T) where {T <: Real} = Quaternion{T}(a.scalar*r, a.x*r, a.y*r, a.z*r);
+*(r::T, a::Quaternion{T}) where {T <: Real} = *(a,r);
+
+"""Quaternion zero and one"""
+zero(a::Quaternion{T}) where {T <: Real} = Quaternion{T}(zero(T), zero(T), zero(T), zero(T));
+one(a::Quaternion{T}) where {T <: Real} = Quaternion{T}(one(T), zero(T), zero(T), zero(T));
+
+"""Invert a Quaternion"""
+function inv(a::Quaternion{T}) where {T <: Real}
+    
+    if a == zero(a)
+        error("Can't invert zero!")
+    else
+        return conj(a)*(1/(a.scalar^2 + a.x^2 + a.y^2 + a.z^2))
+    end
+end
+
+"""Quaternion division"""
+/(a::Quaternion{T}, b::Quaternion{T}) where{T <: Real} = a*inv(b)
+    
+"""Division by scalar"""
+/(a::Quaternion{T}, r::T) where {T <: Real} = Quaternion{T}(a.scalar/r, a.x/r, a.y/r, a.z/r)
+/(a::Quaternion{T}, r::T) where {T <: Integer} = Quaternion(a.scalar/r, a.x/r, a.y/r, a.z/r)
+
+"""Obtain vector and scalar parts of a Quaternion"""
+vectorpart(a::Quaternion{T}) where {T <: Real} = [a.x,a.y,a.z]
+scalarpart(a::Quaternion{T}) where {T <: Real} = a.scalar
+    
+"""Convert vector or scalar to Quaternion"""
+function quaternion(a::Array{T,1}) where {T <: Real}
+    if length(a) == 3
+        return Quaternion{T}(zero(a[1]), a[1], a[2], a[3])
+    elseif length(a) == 4
+        return Quaternion{T}(a[1], a[2], a[3], a[4])
+    else
+        error("Vector must be of length 3 or 4 to convert to Quaternion")
+    end
+end
+
+quaternion(a::T) where {T <: Real} = Quaternion{T}(a, zero(a), zero(a), zero(a))
+
+"""Rotate a point in R3 about an axis by a given angle using quaternions."""
+function rotateaxis(point::Array{T,1}, 
+                    axis::Array{T,1},
+                    angle::T) where {T <: AbstractFloat}
+                
+    if (length(axis) == 3) && (length(point) == 3)
+        axisnorm::T = sqrt(sum(axis.^2))
+        two::T = one(T) + one(T)
+        rot::Quaternion{T} = Quaternion{T}(
+            cos(angle/two),
+            sin(angle/two)*axis[1]/axisnorm,
+            sin(angle/two)*axis[2]/axisnorm,
+            sin(angle/two)*axis[3]/axisnorm
+            )
+        
+        return vectorpart(rot * quaternion(point) * conj(rot))
+    else
+        error("Point and axis must live in R3!")
+    end
+end;
+                
+                
